4533aeda47a1358de19553eef78e034fd1fcf127
Obak3 <Kchristopher1959@gmail.com>
2022-02-12 23:27:56 -0500

    Add other approach with a map

--- Questions/E_ContainsDuplicate.py
+++ Questions/E_ContainsDuplicate.py
@@ -6,3 +6,16 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(nums) != len(set(nums))
 
+# OR
+
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        tracking = {}
+
+        for i in nums:
+            if i in tracking:
+                return True
+            else:
+                tracking[i] = nums.index(i)
+
+        return False
+
